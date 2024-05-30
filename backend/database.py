af9bb880ed9d1d4b8ad407a0b870e5d670668b28
Aditya <adityakeerthi05@gmail.com>
2024-05-30 15:22:42 -0400

    feat(db): setup different env urls

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,34 @@
+import os
 import psycopg2
+from dotenv import load_dotenv
 
 def use_conn():
   # Change later for prod db
-  return psycopg2.connect('postgresql://postgres:postgres@postgres/postgres')
+  ENV = os.getenv('ENV')
+  if ENV == 'local':
+    DB_URL = os.getenv('LOCAL_DB_URL')
+  elif ENV == 'dev':
+    DB_URL = os.getenv('DEV_DB_URL')
+  elif ENV == 'prod':
+    DB_URL = os.getenv('PROD_DB_URL')
+  else:
+    print("Error: Invalid connection")
+    return
+  return psycopg2.connect(DB_URL)
+
+def test_neon():
+  conn = use_conn()
+  cursor = conn.cursor()
+
+  cursor.execute('SELECT NOW();')
+  time = cursor.fetchone()[0]
+  cursor.execute('SELECT version();')
+  version = cursor.fetchone()[0]
+  cursor.close()
+  conn.close()
+  # Print the results
+  print('Current time:', time)
+  print('PostgreSQL version:', version)
 
 def execute_query(query):
   with open('./queries/'+query, 'r') as sql_file:
