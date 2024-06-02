e7ae063afb53e67184b895bdd1aeb1793d28c433
Aditya <adityakeerthi05@gmail.com>
2024-06-01 23:09:02 -0400

    feat(populate_table): add `populate_table` functionality + populate `circuits` table

---
+++ backend/modules/populate_table.py
@@ -0,0 +1,24 @@
+from database import use_conn
+
+def populate_table(table_name: str, csv_path: str):
+  conn = use_conn()
+  cursor = conn.cursor()
+  try:
+    with open(csv_path, 'r') as csv_file:
+      lines = csv_file.readlines()
+      for line in lines[1:]:
+        line = line.strip()
+        line = line.replace('"', "'")
+        line = line.replace(r"\N", "NULL")
+        query = f"INSERT INTO {table_name} VALUES ({line})"
+        cursor.execute(query)
+
+    conn.commit()
+    res = f"Transaction successful"
+  except Exception as e:
+    conn.rollback()
+    res = f"An error occured: {e}"
+  finally:
+    cursor.close()
+    conn.close()
+    return res
--- backend/routers/circuits.py
+++ backend/routers/circuits.py
@@ -1,4 +1,6 @@
+import os
 from fastapi import APIRouter, Depends, HTTPException
+from modules.populate_table import populate_table
 
 router = APIRouter(
   prefix="/circuits",
@@ -6,6 +8,11 @@ router = APIRouter(
   responses={404: {"description": "Not found"}},
 )
 
+current_dir = os.path.dirname(__file__)
+csv_path = os.path.join(current_dir, '..', 'queries', 'data', 'circuits.csv')
+csv_path = os.path.normpath(csv_path)
+
 @router.get("/populate")
 async def populate_circuits():
-  return {"testing": "yoyo circuits here!"}
+  result = populate_table('circuits', csv_path)
+  return {"testing": "yoyo circuits here!" + str(result)}
