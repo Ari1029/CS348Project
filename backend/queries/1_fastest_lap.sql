b77aa1eae8f822257b1ac1f1fc21dcfe69fe114f
Alex Zhu <87958079+probablyalexzhu@users.noreply.github.com>
2024-06-19 17:42:07 -0400

    add 1_fastest_lap query

---
+++ backend/queries/1_fastest_lap.sql
@@ -0,0 +1,10 @@
+-- WE HAVE r.name = 'Monaco Grand Prix', d.surname = 'Hamilton', d.forename = 'Lewis' HARDCODED FOR NOW
+SELECT min(milliseconds)
+FROM laptimes l, races r, drivers d
+WHERE
+    r.name = 'Monaco Grand Prix' AND
+    r.year = 2023 AND
+    d.surname = 'Hamilton'AND
+    d.forename = 'Lewis' AND
+    l.raceid = r.raceid AND 
+    d.driverid = l.driverid
