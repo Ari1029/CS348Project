e6941ecf351898016790b17a1f940579ae976e07
Cheney Cai <cheneycai2004@hotmail.com>
2024-06-19 17:28:00 -0400

    Create 3_most_raced_against.sql

---
+++ backend/queries/3_most_raced_against.sql
@@ -0,0 +1,25 @@
+-- WE HAVE driverid = 1 HARDCODED FOR NOW
+WITH racer_x_races AS (
+  SELECT raceid
+  FROM results
+  WHERE driverid = 1
+),
+
+race_participants AS (
+  SELECT r1.raceid, results.driverid
+  FROM racer_x_races r1, results
+  WHERE  r1.raceid = results.raceid
+    AND results.driverid != 1
+),
+
+driver_race_counts AS (
+    SELECT r.driverid, COUNT(*) AS race_count
+    FROM results r
+    GROUP BY r.driverid
+)
+
+SELECT d.driverid, drc.race_count
+FROM driver_race_counts drc, drivers d
+WHERE drc.driverid = d.driverid
+ORDER BY drc.race_count DESC
+LIMIT 3;
