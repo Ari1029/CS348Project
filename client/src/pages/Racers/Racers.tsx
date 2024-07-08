4b8ebdbe8bf75deeee2537c9d4a8365e39a993f1
Alex Zhu <87958079+probablyalexzhu@users.noreply.github.com>
2024-07-07 23:33:51 -0400

    feat: consecutive wins

--- client/src/pages/Racers/Racers.tsx
+++ client/src/pages/Racers/Racers.tsx
@@ -2,9 +2,11 @@ import { useEffect, useState } from 'react';
 import { Box, Typography } from '@mui/material';
 import FastestLap from './FastestLap';
 import f1Theme from 'styles/theme';
+import ConsecutiveWins from './ConsecutiveWins';
 
 export const Racers = () => {
   const [fastestLap, setFastestLap] = useState<number>(0);
+  const [consecutiveWins, setConsecutiveWins] = useState([[0, 0]]);
 
   return (
     <Box sx={{p: 3}}>
@@ -14,10 +16,14 @@ export const Racers = () => {
           <FastestLap fastestLap={fastestLap} setFastestLap={setFastestLap} />
         </Box>
         <Box>
-            <h1>Average Position ;)</h1>
+            <h1>Average Position</h1>
         </Box>
         <Box>
             <h1>Most Raced Against</h1>
+        </Box>
+        <Box sx={{ border: 1, borderRadius: 2, p: 2 }}>
+          <Typography sx={{ fontSize: "32px", fontWeight: 600, mb: 2 }}>Consecutive Wins</Typography>
+          <ConsecutiveWins consecutiveWins={consecutiveWins} setConsecutiveWins={setConsecutiveWins} />
         </Box>
       </Box>
     </Box>
