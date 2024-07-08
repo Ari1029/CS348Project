1e9bd317f3498ff2b1043ff305b11f21fd9c4c06
Alex Zhu <87958079+probablyalexzhu@users.noreply.github.com>
2024-07-07 22:00:07 -0400

    feat: top 3 constructors' circuits

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import Topbar from "components/Topbar"
 import { Box } from "@mui/material"
 import axios from "axios"
 import Racers from "pages/Racers/Racers"
+import Constructors from "pages/Constructors/Constructors"
 
 function App() {
   axios.defaults.baseURL = "http://localhost:8000";
@@ -13,7 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={ <Home/> } />
         <Route path="/racers" element={ <Racers/> } />
-        <Route path="/constructors" element={ <></> } />
+        <Route path="/constructors" element={ <Constructors/> } />
       </Routes>
     </Box>
   )
---
+++ client/src/pages/Constructors/Constructors.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+import { Box, Typography } from '@mui/material';
+import { getBestCircuitForConstructor } from 'api/F1Api';
+import TopThreeCircuits from './TopThreeCircuits';
+
+export const Constructors = () => {
+  const [constructorName, setConstructorName] = useState("");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await getBestCircuitForConstructor(constructorName);
+        console.log(response);
+        setData(response["message"]);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getData();
+  }, []);
+
+  return (
+    <Box sx={{p: 3}}>
+      <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+        <Box sx={{ border: 1, borderRadius: 2, p: 2 }}>
+          <Typography sx={{ fontSize: "32px", fontWeight: 600, mb: 2 }}>Top Three Constructors' Circuits</Typography>
+          <TopThreeCircuits data={data} setData={setData} />
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+
+export default Constructors;
