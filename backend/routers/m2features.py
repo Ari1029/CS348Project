1b81be1eec1795e3d14698a5552068a6e906e4b1
Aditya <adityakeerthi05@gmail.com>
2024-07-07 21:31:28 -0400

    refactor: renaming queries

--- backend/routers/m2features.py
+++ backend/routers/m2features.py
@@ -25,7 +25,7 @@ async def fastest_lap(fastest_lap_query: FastestLapQuery):
 
 @router.post("/best_circuits_for_constructor")
 async def best_circuits_for_constructor(best_circuits_for_constructor_query: BestCircuitsForConstructor):
-  res = make_query('best_circuits_for_constructor.sql', {
+  res = make_query('4_best_circuits_for_constructor.sql', {
     'constructor_name': best_circuits_for_constructor_query.constructor_name
   })
   return {
@@ -34,14 +34,14 @@ async def best_circuits_for_constructor(best_circuits_for_constructor_query: Bes
 
 @router.get("/avg_pos_for_racer")
 async def avg_pos_for_racer():
-  res = make_query('avg_pos_for_racer.sql')
+  res = make_query('3_avg_pos_for_racer.sql')
   return {
     "message": res
   }
 
 @router.post("/most_raced_against")
 async def most_raced_against(most_raced_against_query: MostRacedAgainstQuery):
-  res = make_query('3_most_raced_against.sql', {
+  res = make_query('2_most_raced_against.sql', {
     'driver_surname': most_raced_against_query.driver_surname,
     'driver_forename': most_raced_against_query.driver_forename
   })
