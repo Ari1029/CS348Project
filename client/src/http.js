4af65cc82c2a1daa4847d66078cfffb9cf8dd0a8
Aditya <adityakeerthi05@gmail.com>
2024-07-23 22:01:49 -0400

    chore: change to backend server

--- client/src/http.js
+++ client/src/http.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const HTTP = axios.create({
-  // baseURL: 'https://f1sql.onrender.com',
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://f1sql.onrender.com',
+  // baseURL: 'http://localhost:8000',
 });
